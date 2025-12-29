6ac971e6b7ae741f37eb0a444d58de47af4c4d74
dlwogus825 <dlwogus825@gmail.com>
2025-12-29 17:23:16 +0900

    한거 올림
    다수 포함

---
+++ HTML/js/1229.js
@@ -0,0 +1,52 @@
+let d;
+const c =30 , PI = 3.14159;
+let b = 20;
+let userName = "alice";
+b = 200;
+userName = "Bob";
+
+const user = { name: "Tom"};
+user.name = "Jerry";
+d = 700;
+
+const obj = new Object();
+const arr1 = [1, 2, 3, 5, 6, 7, 8, "a", "b"];
+const func1 = function() {
+    alert('aaaaa');
+};
+
+// func1();
+
+let c1 = 10;
+c1 -= 5;
+console.log(c1);
+console.log(c1++);
+console.log(++c1);
+console.log(--c1);
+console.log(c1--);
+c1 += c1;
+console.log(c1);
+
+c1 -= c1;
+console.log(c1);
+
+c1 *= c1;
+console.log(c1);
+
+c1 /= c1;
+console.log(c1);
+
+c1 %= c1;
+console.log(c1);
+
+let c2 = 5;
+console.log(c2 > 3);
+
+console.log(c2 < 3);
+console.log(c2 == 3);
+console.log(c2 === 3);
+console.log(c2 && 3);
+console.log(c2 || 3);
+console.log(c2 >= 3);
+console.log(c2 != 3);
+console.log(c2 !== 3);
