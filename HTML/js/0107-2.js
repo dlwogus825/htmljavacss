09221a378e212d1680f64c70e66431ce9b0e96d3
dlwogus825 <dlwogus825@gmail.com>
2026-01-08 19:09:19 +0900

    asdf
    asdf

--- HTML/js/0107-2.js
+++ HTML/js/0107-2.js
@@ -1,53 +1,100 @@
 class DOMControlTest {
   #users = [
-    { id: 1, name: "김철수", kor: 90, eng: 85, mat: 88, clss: "3-1" },
-    { id: 2, name: "이영희", kor: 78, eng: 69, mat: 91, clss: "3-3" },
-    { id: 3, name: "박민수", kor: 75, eng: 69, mat: 82, clss: "3-2" },
+    {
+      id: 1,
+      name: "김철수",
+      kor: 90,
+      eng: 85,
+      mat: 88,
+      clss: "3-1",
+    },
+    {
+      id: 2,
+      name: "이영희",
+      kor: 78,
+      eng: 69,
+      mat: 91,
+      clss: "3-3",
+    },
+    {
+      id: 3,
+      name: "박민수",
+      kor: 75,
+      eng: 69,
+      mat: 82,
+      clss: "3-2",
+    },
   ];
 
   printHtml() {
-    $("#ulList .dataLi").remove();
-
+    $("#ulList").empty();
     this.#users.forEach((student) => {
       $("#ulList").append(this.getOneHtmlStudent(student));
     });
   }
 
   getOneHtmlStudent(student) {
-    return `
-      <li class="dataLi">
-        <div>${student.id}</div>
-        <div>${student.name}</div>
-        <div>${student.kor}</div>
-        <div>${student.eng}</div>
-        <div>${student.mat}</div>
-        <div>${student.clss}</div>
-      </li>
-    `;
+    let oneHtml = `
+<div class="dataLi oneRow">
+  <div>
+    <div>${student.id}</div>
+  </div>
+  <div>
+    <div>${student.name}</div>
+  </div>
+  <div>
+    <div>${student.kor}</div>
+  </div>
+  <div>
+    <div>${student.eng}</div>
+  </div>
+  <div>
+    <div>${student.mat}</div>
+  </div>
+  <div>
+    <div>${student.clss}</div>
+  </div>
+</div>`;
+    return oneHtml;
   }
 
- addStudent() {
-  const student = {
-    id: Number($("#id").val()),
-    name: $("#name").val(),
-    kor: Number($("#kor").val()),
-    eng: Number($("#eng").val()),
-    mat: Number($("#mat").val()),
-    clss: $("#clss").val(),
-  };
-
-  this.#users.push(student);
-
-  $("#id, #name, #kor, #eng, #mat, #clss").val("");
-}
-
+  addStudent() {
+    /*
+    1번.
+    <input type="number" id="id" />
+    <input type="text" id="name" />
+    ....
+    <input type="text" id="clss" /> 입력박스들
+    의 value 값을 가져와서 아래의 형태로 javascript 객체로 만들어야 한다.
+    {
+      id: 3,
+      name: "박민수",
+      kor: 75,
+      eng: 69,
+      mat: 82,
+      clss: "3-2",
+    }
+    2번
+      1번에서 만든 객체를 this.#users 배열 뒤의 요소에 추가해야 한다.
+    */
+    let tmp = {
+      id: $("#id").val(),
+      name: "박민수",
+      kor: 75,
+      eng: 69,
+      mat: 82,
+      clss: "3-2",
+    };
+    this.#users.push(tmp);
+  }
 }
 
 $(function () {
-  const domCtrl = new DOMControlTest();
+  let domCtrl = new DOMControlTest();
   domCtrl.printHtml();
 
-  $("#btnAddStudent").on("click", function () {
+  $("#btnAddStudent").click(function (e) {
+    e.preventDefault();
     domCtrl.addStudent();
     domCtrl.printHtml();
   });
--- HTML/js/boksup2.js
+++ HTML/js/boksup2.js
@@ -1,84 +1,63 @@
-//삼항 연산자란?
-//(조건) ? value1 : value1;
+const container = document.getElementById('container');
 
+// 부모 노드
+console.log("container.parentNode, container.parentElement");
+console.log(container.parentNode);       // body
+console.log(container.parentElement);    // body
 
-// 조건이 참인경우 물음표 바로 옆에 있는 value1이 실행되며 거짓일경우 value2가 실행된다
+// 자식 노드
+console.log("container.childNodes");
+console.log(container.childNodes);       // NodeList (텍스트 노드 포함)
+console.log("container.children");
+console.log(container.children);         // HTMLCollection (요소만)
+console.log("container.firstChild, container.firstElementChild, container.lastChild, container.lastElementChild");
+console.log(container.firstChild);       // 첫 번째 노드
+console.log(container.firstElementChild); // 첫 번째 요소
+console.log(container.lastChild);        // 마지막 노드
+console.log(container.lastElementChild); // 마지막 요소
 
+// 형제 노드
+const h1 = container.querySelector('h1');
+console.log("h1.nextSibling, h1.nextElementSibling, h1.previousSibling, h1.previousElementSibling");
+console.log(h1.nextSibling);            // 다음 노드 (텍스트 노드일 수 있음)
+console.log(h1.nextElementSibling);     // 다음 요소 (p.text)
+console.log(h1.previousSibling);        // 이전 노드
+console.log(h1.previousElementSibling); // 이전 요소
 
-        let x ;
-        let y = 10; 
-
-        x = 20;
-
-        let z = x > y ? "x가 y보다 더 크다" : "X가 y보다 더 작다";
-
-
-        console.log(z);
-
-
-
-        let wentris = 100;
-        let leejaehean = 5;
-        
-        let whowinq = wentris > leejaehean ? "웬트리스는 위대하다 당연히" : "이재현은 분발했다 여전히 웬트리스가 위대하지만";
-        console.log(whowinq);   
-
-const array01 = [5, 2, 3, 8, 2, 1, 8, 9, 11, 17, 20, 552, 281, 3, 91];
-
-function printOk(name, number, baseNum) {
-  (number % baseNum === 0) ? console.log(`${baseNum}의 배수, test01 ${name} : ${number}`) : {};
+// 자식 노드 순회
+console.log("for child of container.children");
+for (let child of container.children) {
+    console.log(child.tagName);
 }
 
-class Study0106 {
-  #array01 = [5, 2, 3, 8, 2, 1, 8, 9, 11, 17, 20, 552, 281, 3, 91];
-
-  
-  
-  test01(baseNum) {
-
-    console.log("baseNum:", baseNum);
-
-    for ( let n = 0; n < this.#array01.length; n++ ) {
-      // 모든 원소중에서 baseNum의 배수 값을 console.log 에 출력한다.
-      printOk("for", this.#array01[n], baseNum);
-      if ( this.#array01[n] % baseNum === 0 ) {
-        console.log(`${baseNum}의 배수, test01 for : ${this.#array01[n]}`);
-      }
-      // 3항 연산자 : (조건) ? 참일때실행 : 거짓일때실행;
-      ( this.#array01[n] % baseNum === 0 ) ? console.log(`${baseNum}의 배수, test01 for : ${this.#array01[n]}`) : {};
+// 모든 노드 순회 (텍스트 노드 포함)
+console.log("for node of container.childNodes");
+for (let node of container.childNodes) {
+    if (node.nodeType === Node.ELEMENT_NODE) {
+        console.log('요소:', node.tagName);
+    } else if (node.nodeType === Node.TEXT_NODE) {
+        console.log('텍스트:', node.nodeValue.trim());
     }
-    for ( const num of this.#array01 ) {
-      // 모든 원소중에서 baseNum의 배수 값을 console.log 에 출력한다.
-      printOk("for of", num, baseNum);
-      if ( num % baseNum === 0 ) {
-        console.log(`${baseNum}의 배수, test01 of : ${num}`);
-      }
-      // 3항 연산자 : (조건) ? 참일때실행 : 거짓일때실행;
-      ( num % baseNum === 0 ) ? console.log(`${baseNum}의 배수, test01 of : ${num}`) : {};
-    }
-    this.#array01.forEach(function(arrayItem, ndx, arr) {
-      // 모든 원소중에서 baseNum의 배수 값을 console.log 에 출력한다.
-      printOk("forEach", arrayItem, baseNum);
-      // if ( arrayItem % baseNum === 0 ) {
-      // console.log(`${baseNum}의 배수, test01 forEach : ${arrayItem}, ${ndx}, [${arr}]`);
-      // }
-      // 3항 연산자 : (조건) ? 참일때실행 : 거짓일때실행;
-      ( arrayItem % baseNum === 0 ) ? console.log(`${baseNum}의 배수, test01 forEach : ${arrayItem}`) : {};
-    });
-  }
-
-  print() {
-    console.log(this.#array01);
-  }
 }
 
-let exam = new Study0106();
+// 문서 정보
+console.log("document infomations");
+console.log(document.title);             // 문서 제목
+console.log(document.URL);               // 현재 URL
+console.log(document.domain);            // 도메인
+console.log(document.documentElement);   // <html> 요소
+console.log(document.body);              // <body> 요소
+console.log(document.head);              // <head> 요소
 
-// 클래스 는 new와 함께 초기화 되지 않으면 쓸수가 없다 초기화 시켜서 examdl이라고 불리게 할거다
+// 문서 상태
+console.log("document readyState");
+console.log(document.readyState);        // "loading", "interactive", "complete"
 
-exam.test01(2);
+// 문서 로드 완료 이벤트
+document.addEventListener('DOMContentLoaded', function() {
+    console.log(document.readyState + ' DOM이 완전히 로드 되었습니다!');
+});
 
-//exam이라는 아까 배열의 초기화되어서 지칭되는 exam에 대한 함수 
-
-//new는 객체를 생성하고 메모리를 할당시켜서 데이터를 저장할 수 있도록 만들며 그리고 생성자를 호출시켜서
-//객체를 초기화 시키고 그렇게 해서 생성된 객체의 메모리 주소를 변환해 변수에 할당시킨다
+window.addEventListener('load', function() {
+    console.log(document.readyState + ' 모든 리소스(폰트, 이미지, 아이콘 등등)가 로드 되었습니다!');
+});
